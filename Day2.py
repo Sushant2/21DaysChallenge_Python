c2c4f421a39ae5bbbc51795b97b37aaa2d54cb0c
Sushant2 <er.sushant.2@gmail.com>
2020-03-28 23:39:54 +0530

    Integer and Float

---
+++ Day2.py
@@ -0,0 +1,63 @@
+# Integers and Floats
+
+num = 1
+print(type(num))
+
+num1 = 3.14
+print(type(num1))
+
+#Arithmetic operations
+
+print(3/2)
+print(3//2)
+print(3%2)
+
+#orders of operations
+print(3 * 2 + 1)
+print(3 * (2 + 1))
+
+#Incrementing
+num = 1
+num = num + 1
+print(num)
+
+# or
+num = 1
+num += 1
+print(num)
+
+# Absolute method()
+
+print(abs(-5))
+print(abs(5))
+
+# Round funtion
+
+print(round(3.75))
+
+# Rounding with the second argument
+print(round(3.755648, 3))
+
+# Comparisons
+
+print(3 == 2)
+print(3 != 2)
+print(3 > 2)
+print(3 < 2)
+print(3 >= 2)
+print(3 <= 2)
+
+# With Strings
+
+num_1 = '100'
+num_2 = '200'
+print(num_1 + num_2)
+
+# we can add them using casting
+num_1 = int(num_1)
+num_2 = int(num_2)
+print(num_1 + num_2)
+
+
+
+
